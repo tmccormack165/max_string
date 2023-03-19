a7f8da45a73f28accd7ca87bbf2a054be298ee57
tmccormack165 <60988784+tmccormack165@users.noreply.github.com>
2023-03-19 15:19:37 +0100

    allowed delimeters to ignore whitespace ex. "," is the same as ", "

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <fstream>
 using namespace std;
 
@@ -35,46 +36,110 @@ bool quit_application(string std_input, char quit_char){
 
     return exit_app;
 }
+/* Count the number of instances of the delimeter in the standard input
+Parameters:
+    - std_input (string): the response from the user
+    - delim (string): the string that partitions entries from the user
+Return:
+    - (int): how many entries the user has sent
+*/
+int count_entries(string std_input, string delim){
+    int n_entries = 0; // how many entries the user has sent
+    int start_pos = 0; // initialize delimeter position to zero
+    int n_delim = 0; // init count to zero
+    int src_length = std_input.length(); // source is the string from standard input
+    int delim_length = delim.length(); // delimeter is a string that separates entries
+    assert(src_length > delim_length); // ensure that the standard input is longer than delim
+    int diff = src_length - delim_length; // record diff 
+
+    while(1){
+        // find() will return the starting position of delim in std_input, or string::npos if no instances occur
+        start_pos = std_input.find(delim, start_pos);
+        // if a delimeter exists and the delimeter does not make up the final set of characters in the string, then increment count
+        if(start_pos != string::npos && start_pos <= diff){
+            n_delim++;
+            start_pos += delim_length; // offset the new starting position so that repeat elements are not counted
+        }
+        else{
+            break; // if either the end of the search string is reached, or no more instances are found, then exit.
+        }
+    }
+    n_entries = n_delim + 1; // add one to n_delim to account for the final entry
+    return n_entries;
+}
 
 int main(){
-    string std_input, longest_string = "", current_string = "";
-    string* s;
-    int *iarr;
-
-    char delim = ' ';
-    char quit_char = 'q';
-    int max_length = 0, current_len = 0;
-    int input_length, nwords;
+    string std_input, delim = ", ", longest_string = "", current_string = "";
+    char char_delim = ',', quit_char = 'q';
+    int n_entries;
     bool is_final;
-    
-    cout << "Enter space-separated words (type " << quit_char << " to quit): ";
+    vector <string> phrases;
+
+    // collect user input into one string
+    cout << "Enter comma-separated strings (type " << quit_char << " to quit): ";
     getline(cin, std_input);
 
+    // check if the user entered the quit character
     if(quit_application(std_input, quit_char)){
         return 0;
     }
 
-    nwords = count_delims(std_input, delim) + 1;
+    int occurences = 0;
+    for(int i = 0; i < std_input.length(); i++){
+        if(std_input[i] == char_delim){
+            while(1){
+                if(std_input[i] != ' ' && std_input[i] != char_delim){
+                    phrases.push_back(current_string);
+                    current_string = "";
+                    current_string += std_input[i];
+                    break;
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        else{
+            current_string += std_input[i];
+        }
+    }
+    phrases.push_back(current_string);
+    for(auto item:phrases){
+        cout << item << endl;
+    }
+
+    return 0;
+
+    
+
+
+
+    /*
+    int max_length = 0, current_len = 0;
+    int input_length,
+
     s = new string[nwords];
     iarr = new int[nwords];
 
     input_length = std_input.length();
 
-    /*
+    
     For each character from standard input:
         - if the char is a delim or the char is the final char
             - if the current length is greater than the max length
                 - update the max length
                 - update the longest string
             - reset the current string and current length
-    */
+    
     int hmr = 0;
     for(int i = 0; i < input_length; i++){
-        current_string += std_input[i];
+       
+        
         is_final = check_final(i, input_length);
-        if(std_input[i] == delim || is_final){
+        if(std_input[i] == delim[0] || is_final){
             if(is_final){
                 current_len += 1;
+                current_string += std_input[i];
             }
             s[hmr] = current_string;
             iarr[hmr] = current_len;
@@ -88,6 +153,7 @@ int main(){
             hmr++;
         }
         else{
+            current_string += std_input[i];
             current_len += 1;
         }
         
@@ -106,4 +172,6 @@ int main(){
     delete[] iarr;
     
     return 0;
+    
+    */
 }
